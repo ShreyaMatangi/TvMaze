1097a243f68a18462f3e688684feb0d4d01d52a8
matangi divakar <shreyagriet@gmail.com>
2020-07-20 13:42:07 +0530

    front end task

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import React from "react";
+import {
+StyleSheet,
+View,
+ActivityIndicator,
+FlatList,
+Text,
+TouchableOpacity
+} from "react-native";
+export default class Source extends React.Component {
+static navigationOptions = ({ navigation }) => {
+return {
+  title: "Source Listing",
+  headerStyle: {backgroundColor: "#fff"},
+  headerTitleStyle: {textAlign: "center",flex: 1}
+ };
+};
+constructor(props) {
+ super(props);
+ this.state = {
+   loading: true,
+   dataSource:[]
+  };
+}
+componentDidMount(){
+fetch("http://api.tvmaze.com/shows/1/episodes")
+.then(response => response.json())
+.then((responseJson)=> {
+  this.setState({
+   loading: false,
+   dataSource: responseJson
+  })
+})
+.catch(error=>console.log(error)) //to catch the errors if any
+}
+FlatListItemSeparator = () => {
+return (
+  <View style={{
+     height: .5,
+     width:"100%",
+     backgroundColor:"rgba(0,0,0,0.5)",
+}}
+/>
+);
+}
+renderItem=(data)=>
+
+<TouchableOpacity style={styles.list}>
+<Text style={styles.lightText}>{data.item.name}</Text>
+</TouchableOpacity>
+
+
+render(){
+ if(this.state.loading){
+  return( 
+    <View style={styles.loader}> 
+      <ActivityIndicator size="large" color="#0c9"/>
     </View>
-  );
+)}
+return(
+ <View style={styles.container}>
+ <Text>Under the Dome Episodes</Text>
+ <FlatList
+    data= {this.state.dataSource}
+    ItemSeparatorComponent = {this.FlatListItemSeparator}
+    renderItem= {item=> this.renderItem(item)}
+    keyExtractor= {item=>item.id.toString()}
+ />
+</View>
+)}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+   },
+  loader:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
+   },
+  list:{
+    paddingVertical: 4,
+    margin: 5,
+    backgroundColor: "#fff"
+   }
 });
